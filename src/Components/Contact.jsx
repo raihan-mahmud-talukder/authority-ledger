44ff45f975c905afe674f2d6bf67e67b02283503
Raihan <msr.raihan0mahmud@gmail.com>
2026-04-16 00:10:27 +0600

    contact left section

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,27 @@
 export const Contact = () => {
     return (
-        <>
-            <h1>Contact</h1>
-        </>
+        <section className="py-16 px-8">
+            <aside className="
+                flex flex-col gap-8
+            ">
+                <h2 className="
+                    font-[Manrope] font-extrabold text-5xl text-[#181c1e] tracking-[-0.2rem] md:tracking-[-2.4px]
+                    ">Ready to build your <span className="text-[#2180ff]">Authority?</span></h2>
+                <p className="
+                    text-lg text-[#44474D]
+                ">Schedule a discovery call with our architectural lead to audit your current performance and map your growth path.</p>
+                <div className="flex gap-6 items-center">
+                    <img src="images/email.png" alt="email"/>
+                    <span className="font-semibold">hello@authorityledger.com</span>
+                </div>
+                <div className="flex gap-6 items-center">
+                    <img src="images/address.png" alt="address"/>
+                    <span className="font-semibold">Financial District, New York</span>
+                </div>
+            </aside>
+            <form>
+
+            </form>
+        </section>
     )
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,25 +5,25 @@ export const Footer = () => {
                 flex flex-col gap-12 md:flex-row md:flex-wrap md:justify-between
             ">
                 <div className="w-64 flex flex-col justify-between gap-4">
-                    <h3>Authority Ledger</h3>
+                    <h4>Authority Ledger</h4>
                     <p className="
                         text-sm text-[#64748B]
                     ">The premier choice for high-growth enterprise marketing and digital architecture.</p>
                 </div>
                 <div className="w-64 flex flex-col justify-between gap-4">
-                    <h3>Company</h3>
+                    <h4>Company</h4>
                     <span>About Us</span>
                     <span>Case Studies</span>
                     <span>Careers</span>
                 </div>
                 <div className="w-64 flex flex-col justify-between gap-4">
-                    <h3>Services</h3>
+                    <h4>Services</h4>
                     <span>Performance SEO</span>
                     <span>Paid Media</span>
                     <span>Brand Identity</span>
                 </div>
                 <div className="w-64 flex flex-col justify-between gap-4">
-                    <h3>Connect</h3>
+                    <h4>Connect</h4>
                     <span>Linked In</span>
                     <span>Instagram</span>
                     <span>Twitter (X)</span>
